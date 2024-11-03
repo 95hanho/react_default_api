7b192befd79d434444fb44e2cc1b1dd7499bcba4
95hanho <hosung6674@naver.com>
2024-11-03 22:12:08 +0900

    init

---
+++ src/main/java/me/_hanho/react_default/ReactDefaultApplication.java
@@ -0,0 +1,13 @@
+package me._hanho.react_default;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactDefaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactDefaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/_hanho/react_default/ReactDefaultApplicationTests.java
@@ -0,0 +1,13 @@
+package me._hanho.react_default;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactDefaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
